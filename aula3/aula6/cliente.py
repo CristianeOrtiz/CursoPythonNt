b75ac3400e709c5ab8b662e35439fb015be93eee
Cristiane Aparecida Pavlak Ortiz <67395@PROWAY.TREINA>
2019-11-18 23:06:39 -0200

    aula final 'teste banco'

---
+++ aula3/aula6/cliente.py
@@ -0,0 +1,8 @@
+class Cliente:
+    def __init__(self, nome, cpf, data_nas, id=None):
+        self.id = id
+        self.nome = nome
+        self.cpf = cpf
+        self.data_nascimento = data_nas
+        
+        
